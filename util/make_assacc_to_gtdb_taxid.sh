6686e7886a78bee540259dac195c16fb6ae4dd6e
jaebeom <jbeom0731@gmail.com>
2021-02-23 20:40:08 +0900

    make_assacc_to_taxid:

--- util/make_assacc_to_gtdb_taxid.sh
+++ util/make_assacc_to_gtdb_taxid.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
 
-meta=$1
-map=$2
+PWD=$(pwd)
+ar_gz="${PWD}/ar.tar.gz"
+bac_gz="${PWD}/bac.tar.gz"
 
-awk -F '\t' '$1 ~ /^(R|G)/{print $1,$55,$111}' "${meta}" | while read -r gtdbacc genbankacc taxid; do
+wget -O "${ar_gz}" https://data.ace.uq.edu.au/public/gtdb/data/releases/latest/ar122_metadata.tar.gz
+wget -O "${bac_gz}" https://data.ace.uq.edu.au/public/gtdb/data/releases/latest/bac120_metadata.tar.gz
+
+ar_meta="${PWD}/$(tar -tf "${ar_gz}")"
+bac_meta="${PWD}/$(tar -tf "${bac_gz}")"
+ar_bac_meta="${PWD}/ar_bac_meta.tsv"
+ar_bac_meta_wTaxIDs="${PWD}/ar_bac_meta_wTaxIDs.tsv"
+map="${PWD}/assacc_to_taxid_gtdb.tsv"
+
+tar -xvf "${ar_gz}"
+tar -xvf "${bac_gz}"
+rm "${ar_gz}"
+rm "${bac_gz}"
+
+cat "${ar_meta}" "${bac_meta}" > "${ar_bac_meta}"
+rm "${ar_meta}"
+rm "${bac_meta}"
+
+./gtdb_to_taxdump.py \
+  -t "${ar_bac_meta}" \
+  https://data.ace.uq.edu.au/public/gtdb/data/releases/latest/ar122_taxonomy.tsv \
+  https://data.ace.uq.edu.au/public/gtdb/data/releases/latest/bac120_taxonomy.tsv \
+  > taxID_info.tsv
+
+awk -F '\t' '$1 ~ /^(R|G)/{print $1,$55,$111}' "${ar_bac_meta_wTaxIDs}" | while read -r gtdbacc genbankacc taxid; do
 	if [[ "$gtdbacc" == "R"* ]]; then
 		echo -e "${gtdbacc:3}\t${taxid}"
 		echo -e "${genbankacc}\t${taxid}"
@@ -11,6 +36,14 @@ awk -F '\t' '$1 ~ /^(R|G)/{print $1,$55,$111}' "${meta}" | while read -r gtdbacc
 	  echo -e "${genbankacc}\t${taxid}"
 	fi
 done > "${map}"
+
+mv "${ar_bac_meta_wTaxIDs}" "../gtdb_taxdmp"
+mv "merged.dmp" "../gtdb_taxdmp"
+mv "names.dmp" "../gtdb_taxdmp"
+mv "nodes.dmp" "../gtdb_taxdmp"
+mv "delnodes.dmp" "../gtdb_taxdmp"
+mv "${map}" "../gtdb_taxdmp"
+mv "taxID_info.tsv" "../gtdb_taxdmp"
 
 #for i in $(awk '$2 ~ /^(R|G)/{print $2"|"$1}' ${taxIdInfo}); do
 #	ASSACC=$(echo $i|cut -d '|' -f1)
--- util/make_assacc_to_ncbi_taxid.sh
+++ util/make_assacc_to_ncbi_taxid.sh
@@ -4,6 +4,7 @@ MAP=$2
 MAP_pre="${MAP}_tmp"
 SUM_TXT="${WDIR}/assembly_summary_genbank.txt"
 SUM_TSV="${WDIR}/assembly_summary_genbank.tsv"
+
 wget -O "${SUM_TXT}" https://ftp.ncbi.nlm.nih.gov/genomes/genbank/assembly_summary_genbank.txt
 sed '1,2d' "${SUM_TXT}" > "${SUM_TSV}"
 rm "${SUM_TXT}"
