7fb62a4cfb148f372670e091783758f74fca28c4
jaebeom <jbeom0731@gmail.com>
2021-03-03 14:29:06 +0900

    s

--- util/make_taxIdList_and_concatenatedGenome.sh
+++ util/make_taxIdList_and_concatenatedGenome.sh
@@ -4,11 +4,11 @@ TAXLIST=$1
 TAX_FILE_SORTED=$2
 GENOME=$3
 
-#printf "Making a taxonomical ID list"
-#awk -F '\t' '{print $1,$2}' "${TAX_FILE_SORTED}" | while read -r tax fname; do
-#  awk -v taxid="${tax}" '/^>/{print taxid}' "${fname}"
-#done > "${TAXLIST}"
-#echo "...done"
+printf "Making a taxonomical ID list"
+awk -F '\t' '{print $1,$2}' "${TAX_FILE_SORTED}" | while read -r tax fname; do
+  awk -v taxid="${tax}" '/^>/{print taxid}' "${fname}"
+done > "${TAXLIST}"
+echo "...done"
 
 printf "Concatenating fasta files"
 awk -F '\t' '{print $2}' "${TAX_FILE_SORTED}"|xargs cat > "${GENOME}"
