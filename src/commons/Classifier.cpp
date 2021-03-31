de09332c640fbbd01aa6fc0bf451e2f65fccfd79
jaebeom <jbeom0731@gmail.com>
2021-03-31 16:43:37 +0900

    a

--- src/commons/Classifier.cpp
+++ src/commons/Classifier.cpp
@@ -250,7 +250,7 @@ void Classifier::startClassify3(const char * queryFileName, const char * targetD
     bool * processedSeqChecker = (bool *)malloc(numOfSeq);
     fill_n(processedSeqChecker, numOfSeq, false);
 
-    QueryKmerBuffer kmerBuffer(200000);
+    QueryKmerBuffer kmerBuffer(kmerBufSize);
     Buffer<Match> matchBuffer(kmerBufSize);
     size_t processedSeqCnt = 0;
     size_t processedKmerCnt = 0;
@@ -532,13 +532,13 @@ void Classifier::linearSearchParallel(QueryKmer * queryKmerList, size_t & queryK
                         targetInfoIdx++;
                     }
 
-                    if(queryKmerList[j].info.sequenceID == 12 && queryKmerList[j].info.frame == 0 && targetKmerCache.size() != 0){
-                        cout<<"Position"<<queryKmerList[j].info.pos<<endl;
-                        for(size_t kk = 0 ; kk < targetKmerCache.size(); kk++){
-                            cout<<targetKmerCache[i]<<endl;
-                        }
-                        cout<<endl;
-                    }
+//                    if(queryKmerList[j].info.sequenceID == 12 && queryKmerList[j].info.frame == 0 && targetKmerCache.size() != 0){
+//                        cout<<"Position"<<queryKmerList[j].info.pos<<endl;
+//                        for(size_t kk = 0 ; kk < targetKmerCache.size(); kk++){
+//                            cout<<targetKmerCache[i]<<endl;
+//                        }
+//                        cout<<endl;
+//                    }
                     ///Compare the current query and the loaded target k-mers and select
                     compareDna(currentQuery, targetKmerCache, startIdxOfAAmatch, selectedMatches, selectedHammings);
                     posToWrite = matchBuffer.reserveMemory(selectedMatches.size());
