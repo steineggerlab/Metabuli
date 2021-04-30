335bc6ba6debc63e80ea9763c27024a1ccbbf6e7
jaebeom <jbeom0731@gmail.com>
2021-04-30 17:22:22 +0900

    a

--- src/commons/Classifier.cpp
+++ src/commons/Classifier.cpp
@@ -631,7 +631,7 @@ TaxID Classifier::chooseBestTaxon2(NcbiTaxonomy & ncbiTaxonomy, const size_t & q
     for(size_t i = 0; i < taxIdList.size(); i++){
         cout<<i<<" "<<int(frame[i])<<" "<<pos[i]<<" "<<taxIdList[i]<<" "<<int(ham[i])<<" "<<redun[i]<<endl;
     }
-    cout<<"coverage: "<<coverage<<"  "<<ncbiTaxonomy.taxonNode(selectedLCA)->rank<<endl;
+    cout<<"coverage: "<<coverage<<"  "<<selectedLCA<<" "<<ncbiTaxonomy.taxonNode(selectedLCA)->rank<<endl;
 
     ///store classification results
     queryList[currentQuery].isClassified = true;
