32e8448b35510a7da1cb0c09af37e2f0ab1a3147
jaebeom <jbeom0731@gmail.com>
2021-05-10 15:40:21 +0900

    a

--- src/workflow/krakenuniq_test.cpp
+++ src/workflow/krakenuniq_test.cpp
@@ -41,7 +41,6 @@ int krakenuniq_test(int argc, const char **argv, const Command &command){
     vector<int> classList;
     int classInt;
     while(getline(readClassification,classString,'\n')){
-        cout<<classString<<endl;
         classInt = stoi(classString);
         if(classInt > 1000000000){
             classList.push_back(child2parent[classInt]);
@@ -54,7 +53,30 @@ int krakenuniq_test(int argc, const char **argv, const Command &command){
     for(int i = 0 ; i<classList.size(); i++){
         cout<<i<< " "<<classList[i]<<endl;
     }
-    
+
+    ///Load query file
+    regex regex1("(GC[AF]_[0-9]*\\.[0-9]*)");
+    smatch assacc;
+    string queryName;
+
+    ifstream query;
+    query.open(queryFileName);
+    string queryLine;
+    vector<string> queryNameList;
+    while(getline(query,queryLine,'\n')){
+        if(queryLine[0] == '>'){
+            regex_search(queryLine, assacc, regex1);
+            queryNameList.push_back(assacc[0]);
+        }else{
+            continue;
+        }
+    }
+
+    for(int i = 0 ; i<queryNameList.size(); i++){
+        cout<<i<< " "<<queryNameList[i]<<endl;
+    }
+
+
     ///Load the mapping file (assacc to taxID)
     const char * mappingFile = "../../gtdb_taxdmp/assacc_to_taxid_gtdb.tsv";
     unordered_map<string, int> assacc2taxid;
