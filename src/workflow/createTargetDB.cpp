5d57d64849609539009f0f1ac278824723026a52
jaebeom <jbeom0731@gmail.com>
2021-06-29 18:22:17 +0900

    a

--- src/workflow/createTargetDB.cpp
+++ src/workflow/createTargetDB.cpp
@@ -109,16 +109,18 @@ int createTargetDB(int argc, const char **argv, const Command &command)
     cout<<"Archaea"<<endl;
     for(auto it = subspeciesCnt.begin(); it != subspeciesCnt.end(); it++){
         if(speciesToBeExcluded.end() != find(speciesToBeExcluded.begin(), speciesToBeExcluded.end(), ncbiTaxonomy.getTaxIdAtRank(it->first, "species"))){
-            if(ncbiTaxonomy.getTaxIdAtRank(it->first, "superkingdom") == 2)
-            cout<<iii++<<" "<<ncbiTaxonomy.taxonNode(it->first)->name<<endl;
+            if(ncbiTaxonomy.getTaxIdAtRank(it->first, "superkingdom") == 2) {
+                cout << iii++ << " " << ncbiTaxonomy.taxonNode(it->first)->name << endl;
+            }
         }
     }
 
     cout<<"Bacteria"<<endl;
     for(auto it = subspeciesCnt.begin(); it != subspeciesCnt.end(); it++){
         if(speciesToBeExcluded.end() != find(speciesToBeExcluded.begin(), speciesToBeExcluded.end(), ncbiTaxonomy.getTaxIdAtRank(it->first, "species"))){
-            if(ncbiTaxonomy.getTaxIdAtRank(it->first, "superkingdom") == 8034)
-                cout<<iii++<<" "<<ncbiTaxonomy.taxonNode(it->first)->name<<endl;
+            if(ncbiTaxonomy.getTaxIdAtRank(it->first, "superkingdom") == 8034) {
+                cout << iii++ << " " << ncbiTaxonomy.taxonNode(it->first)->name << endl;
+            }
         }
     }
 
