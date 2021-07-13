716a4205485063cd3c334c0937cd4bbd5b43c258
jaebeom <jbeom0731@gmail.com>
2021-07-14 01:39:59 +0900

    a

--- src/workflow/kaiju_scoring.cpp
+++ src/workflow/kaiju_scoring.cpp
@@ -123,7 +123,7 @@ int exclusiontest(int argc, const char **argv, const Command &command){
     for(size_t i = 0; i < queryNameList.size(); i++){
         if (assacc2taxid.count(queryNameList[i])) {
             taxid = assacc2taxid[queryNameList[i]];
-            while(cladeCnt[taxid] != 1)
+        
         } else{
             cout << queryNameList[i] << " is not in the mapping file" << endl;
             rightAnswers.push_back(-1);
