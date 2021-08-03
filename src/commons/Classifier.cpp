980e25248f1981bfe599efaf68dfafa9c09cde71
jaebeom <jbeom0731@gmail.com>
2021-08-03 15:01:21 +0900

    a

--- src/commons/Classifier.cpp
+++ src/commons/Classifier.cpp
@@ -633,23 +633,23 @@ TaxID Classifier::chooseBestTaxon(NcbiTaxonomy & ncbiTaxonomy, const size_t & qu
 
     ///TODO optimize strain specific classification criteria
     //Strain classification only for high coverage with LCA of species level
-    if(selectedPercent > 0.8 && NcbiTaxonomy::findRankIndex(ncbiTaxonomy.taxonNode(selectedLCA)->rank) == 4){ /// There are more strain level classifications with lower coverage threshold, but also with more false postives. 0.8~0.85 looks good.
-        int strainCnt = 0;
-        unordered_map<TaxID, int> strainMatchCnt;
-        TaxID strainTaxId;
-
-        for(size_t cs = 0; cs < matchCombi.size(); cs++ ){
-            for(size_t k = matchCombi[cs].beginIdx ; k < matchCombi[cs].endIdx + 1; k++ ){
-                temp = matchList[k].taxID;
-                if(selectedLCA != temp && ncbiTaxonomy.IsAncestor(selectedLCA, temp)){
-                    strainMatchCnt[temp] ++;
-                }
-            }
-        }
-        if(strainMatchCnt.size() == 1 && strainMatchCnt.begin()->second > 1){
-            selectedLCA = strainMatchCnt.begin()->first;
-        }
-    }
+//    if(selectedPercent > 0.8 && NcbiTaxonomy::findRankIndex(ncbiTaxonomy.taxonNode(selectedLCA)->rank) == 4){ /// There are more strain level classifications with lower coverage threshold, but also with more false postives. 0.8~0.85 looks good.
+//        int strainCnt = 0;
+//        unordered_map<TaxID, int> strainMatchCnt;
+//        TaxID strainTaxId;
+//
+//        for(size_t cs = 0; cs < matchCombi.size(); cs++ ){
+//            for(size_t k = matchCombi[cs].beginIdx ; k < matchCombi[cs].endIdx + 1; k++ ){
+//                temp = matchList[k].taxID;
+//                if(selectedLCA != temp && ncbiTaxonomy.IsAncestor(selectedLCA, temp)){
+//                    strainMatchCnt[temp] ++;
+//                }
+//            }
+//        }
+//        if(strainMatchCnt.size() == 1 && strainMatchCnt.begin()->second > 1){
+//            selectedLCA = strainMatchCnt.begin()->first;
+//        }
+//    }
 
     cout<<"# "<<currentQuery<<endl;
     for(size_t i = 0; i < taxIdList.size(); i++){
