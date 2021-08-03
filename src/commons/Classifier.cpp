9935fa3c64778275872ecf924085d83f9c140510
jaebeom <jbeom0731@gmail.com>
2021-08-03 17:10:30 +0900

    a

--- src/commons/Classifier.cpp
+++ src/commons/Classifier.cpp
@@ -540,9 +540,9 @@ void Classifier::analyseResultParallel(NcbiTaxonomy & ncbiTaxonomy, vector<Seque
 TaxID Classifier::chooseBestTaxon(NcbiTaxonomy & ncbiTaxonomy, const size_t & queryLength, const int & currentQuery, const size_t & offset, const size_t & end, Match * matchList, Query * queryList){
 
     TaxID selectedTaxon;
-//    for(int i = offset; i < end + 1; i++){
-//        cout<<int(matchList[i].frame)<<" "<<matchList[i].position<<" "<<matchList[i].taxID<<endl;
-//    }
+    for(int i = offset; i < end + 1; i++){
+        cout<<int(matchList[i].frame)<<" "<<matchList[i].position<<" "<<matchList[i].taxID<<endl;
+    }
 
     //get the best genus for current query
     vector<ConsecutiveMatches> matchCombi;
