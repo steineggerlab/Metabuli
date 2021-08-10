314af47234fb8734a3c24642f4ba2405512a926c
jaebeom <jbeom0731@gmail.com>
2021-08-10 16:23:43 +0900

    a

--- src/commons/SeqIterator.cpp
+++ src/commons/SeqIterator.cpp
@@ -193,10 +193,10 @@ bool SeqIterator::translateBlock(const char * seq, PredictedBlock & block){
     size_t blockLength = block.end - block.start + 1;
     cout<<"L: "<<blockLength<<endl;
     aaFrames[0].reserve(blockLength / 3 + 1);
-    cout<<"hi"<<endl;
+
     if(block.strand == 1){
-        for(size_t i = 0; i < blockLength - 2; i = i + 3){
-            if(block.start+i > strlen(seq) - 1){
+        for(size_t i = block.start ; i + 2 <= block.end ; i = i + 3){
+            if( i + 2 > strlen(seq) - 1){
                 cout<<"over flow 1"<<endl;
                 cout<<"last index of seq: "<<strlen(seq) - 1 <<endl;
                 cout<<"block.start: "<<block.start<<endl;
@@ -205,11 +205,11 @@ bool SeqIterator::translateBlock(const char * seq, PredictedBlock & block){
                 cout<<"L :"<<blockLength<<endl;
                 return false;
             }
-            aaFrames[0].push_back(nuc2aa[nuc2int(atcg[seq[block.start + i]])][nuc2int(atcg[seq[block.start+i+1]])][nuc2int(atcg[seq[block.start+i+2]])]);
+            aaFrames[0].push_back(nuc2aa[nuc2int(atcg[seq[i]])][nuc2int(atcg[seq[i+1]])][nuc2int(atcg[seq[i+2]])]);
         }
     }else{
-        for(size_t i = 0; i < blockLength - 2; i = i + 3){
-            if(block.end-i-2 < 0){
+        for(size_t i = block.end; i - 2 >= block.start; i = i - 3){
+            if( i - 2 < 0){
                 cout<<"over flow 2"<<endl;
                 cout<<"last index of seq: "<<strlen(seq) - 1 <<endl;
                 cout<<"block.start: "<<block.start<<endl;
@@ -217,9 +217,36 @@ bool SeqIterator::translateBlock(const char * seq, PredictedBlock & block){
                 cout<<"L :"<<blockLength<<endl;
                 return false;
             }
-            aaFrames[0].push_back(nuc2aa[nuc2int(iRCT[atcg[seq[block.end - i]]])][nuc2int(iRCT[atcg[seq[block.end-i-1]]])][nuc2int(iRCT[atcg[seq[block.end-i-2]]])]);
+            aaFrames[0].push_back(nuc2aa[nuc2int(iRCT[atcg[seq[i]]])][nuc2int(iRCT[atcg[seq[i-1]]])][nuc2int(iRCT[atcg[seq[i-2]]])]);
         }
     }
+
+//    if(block.strand == 1){
+//        for(size_t i = 0; i < blockLength - 2; i = i + 3){
+//            if(block.start+i > strlen(seq) - 1){
+//                cout<<"over flow 1"<<endl;
+//                cout<<"last index of seq: "<<strlen(seq) - 1 <<endl;
+//                cout<<"block.start: "<<block.start<<endl;
+//                cout<<"bloc.end: "<<block.end<<endl;
+//                cout<<"i :"<<i<<endl;
+//                cout<<"L :"<<blockLength<<endl;
+//                return false;
+//            }
+//            aaFrames[0].push_back(nuc2aa[nuc2int(atcg[seq[block.start + i]])][nuc2int(atcg[seq[block.start+i+1]])][nuc2int(atcg[seq[block.start+i+2]])]);
+//        }
+//    }else{
+//        for(size_t i = 0; i < blockLength - 2; i = i + 3){
+//            if(block.end-i-2 < 0){
+//                cout<<"over flow 2"<<endl;
+//                cout<<"last index of seq: "<<strlen(seq) - 1 <<endl;
+//                cout<<"block.start: "<<block.start<<endl;
+//                cout<<"i :"<<i<<endl;
+//                cout<<"L :"<<blockLength<<endl;
+//                return false;
+//            }
+//            aaFrames[0].push_back(nuc2aa[nuc2int(iRCT[atcg[seq[block.end - i]]])][nuc2int(iRCT[atcg[seq[block.end-i-1]]])][nuc2int(iRCT[atcg[seq[block.end-i-2]]])]);
+//        }
+//    }
     return true;
 }
 
