471356ca1e3e426f5c506b37141a21a8d12d7c19
jaebeom <jbeom0731@gmail.com>
2021-08-18 15:42:04 +0900

    a

--- src/workflow/classify.cpp
+++ src/workflow/classify.cpp
@@ -11,22 +11,14 @@ int classify(int argc, const char **argv, const Command& command)
     LocalParameters &par = LocalParameters::getLocalInstance();
     par.parseParameters(argc, argv, command, false, Parameters::PARSE_ALLOW_EMPTY, 0);
 
-    Classifier classifier;
     const char * queryFileName = par.filenames[0].c_str();
-
     const string databaseDirectory = par.filenames[1];
     const string targetDiffIdxFileName = databaseDirectory+"/diffIdx";
     const string targetInfoFileName = databaseDirectory+"/info";
     string taxIdFileName = databaseDirectory+"/taxID_list";;
     const string diffIdxSplitFileName = databaseDirectory+"/split";;
 
-//    const string targetDiffIdxFileName = par.filenames[1];
-//    const string targetInfoFileName = par.filenames[2];
-//    string taxIdFileName = par.filenames[3];
-//    const string diffIdxSplitFileName = par.filenames[4];
-
-
-
+    //Load the taxonomical ID list
     FILE * taxIdFile;
     if((taxIdFile = fopen(taxIdFileName.c_str(),"r")) == NULL){
         cout<<"Cannot open the taxID list file."<<endl;
@@ -39,9 +31,9 @@ int classify(int argc, const char **argv, const Command& command)
         taxIdList.push_back(atol(taxID));
     }
     fclose(taxIdFile);
-
+    
+    Classifier classifier;
     classifier.startClassify(queryFileName, targetDiffIdxFileName.c_str(), targetInfoFileName.c_str(), diffIdxSplitFileName.c_str(), taxIdList, par);
-   // classifier.startClassify(queryFileName, targetDiffIdxFileName.c_str(), targetInfoFileName.c_str(), diffIdxSplitFileName.c_str(), taxIdList, par);
 
     return 0;
 }
