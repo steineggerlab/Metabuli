733cf093766fb9ff41b5b10551a8b11b680dbdfe
jaebeom <jbeom0731@gmail.com>
2021-09-06 22:30:31 +0900

    a

--- src/commons/SeqIterator.cpp
+++ src/commons/SeqIterator.cpp
@@ -186,7 +186,7 @@ bool SeqIterator::translateBlock(const char * seq, PredictedBlock & block){
         for(int i = block.start ; i + 2 <= block.end ; i = i + 3){
             aaFrames[0].push_back(nuc2aa[nuc2int(atcg[seq[i]])][nuc2int(atcg[seq[i+1]])][nuc2int(atcg[seq[i+2]])]);
             if(aaFrames[0].back() == - 1){
-                cout<<"NN"<<endl;
+                cout<<"NF"<<endl;
                 cout<<seq[i]<<seq[i+1]<<seq[i+2]<<endl;
                 cout<<atcg[seq[i]]<<atcg[seq[i+1]]<<atcg[seq[i+2]]<<endl;
                 cout<<int(nuc2int(atcg[seq[i]]))<<int(nuc2int(atcg[seq[i+1]]))<<int(nuc2int(atcg[seq[i+2]]))<<endl;
@@ -200,7 +200,7 @@ bool SeqIterator::translateBlock(const char * seq, PredictedBlock & block){
         for(int i = block.end; i >= (int)block.start + 2; i = i - 3){
             aaFrames[0].push_back(nuc2aa[nuc2int(iRCT[atcg[seq[i]]])][nuc2int(iRCT[atcg[seq[i-1]]])][nuc2int(iRCT[atcg[seq[i-2]]])]);
             if(aaFrames[0].back() == - 1){
-                cout<<"NN"<<endl;
+                cout<<"NR"<<endl;
                 cout<<seq[i]<<seq[i-1]<<seq[i-2]<<endl;
                 cout<<atcg[seq[i]]<<atcg[seq[i-1]]<<atcg[seq[i-2]]<<endl;
                 cout<<int(nuc2int(atcg[seq[i]]))<<int(nuc2int(atcg[seq[i-1]]))<<int(nuc2int(atcg[seq[i-2]]))<<endl;
