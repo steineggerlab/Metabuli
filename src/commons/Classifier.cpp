78c34de30443c766e10e092fc141fdde7a4704bd
jaebeom <jbeom0731@gmail.com>
2021-09-25 14:14:39 +0900

    print

--- src/commons/Classifier.cpp
+++ src/commons/Classifier.cpp
@@ -237,9 +237,9 @@ void Classifier::linearSearchParallel(QueryKmer * queryKmerList, size_t & queryK
         }
     }
 
-    for(int i = 0 ; i < numOfDiffIdxSplits; i++){
-        cout<<diffIdxSplits.data[i].infoIdxOffset<<" "<<diffIdxSplits.data[i].diffIdxOffset<<" "<<diffIdxSplits.data[i].ADkmer<<endl;
-    }
+//    for(int i = 0 ; i < numOfDiffIdxSplits; i++){
+//        cout<<diffIdxSplits.data[i].infoIdxOffset<<" "<<diffIdxSplits.data[i].diffIdxOffset<<" "<<diffIdxSplits.data[i].ADkmer<<endl;
+//    }
 
     cout<<"Filtering out meaningless target splits ... done"<<endl;
 
