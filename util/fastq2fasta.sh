70fab452de5795d4c9f1bcc3f11691491f9cfb0d
jaebeom <jbeom0731@gmail.com>
2021-10-08 19:44:37 +0900

    fastq2fasta

--- util/fastq2fasta.sh
+++ util/fastq2fasta.sh
@@ -2,4 +2,15 @@
 infile=$1
 outfile=$2
 
+awk '{if(NR%4==1) {split($0, array, "_"); printf(">%s\n",array[5]);} else if(NR%4==2) print;}' "${infile}" > "${outfile}"
+
+#awk -F '\t' '$1 ~ /^(R|G)/{print $1,$55,$111}' "${ar_bac_meta_wTaxIDs}" | while read -r gtdbacc genbankacc taxid; do
+#	if [[ "$gtdbacc" == "R"* ]]; then
+#		echo -e "${gtdbacc:3}\t${taxid}"
+#		echo -e "${genbankacc}\t${taxid}"
+#	else
+#	  echo -e "${genbankacc}\t${taxid}"
+#	fi
+#done > "${map}"
+
 awk '{if(NR%4==1) {printf(">%s\n",substr($0,2));} else if(NR%4==2) print;}' "${infile}" > "${outfile}"
