25689968a294f127c518824851ca2a79408fb5c5
jaebeom <jbeom0731@gmail.com>
2021-10-23 17:28:19 +0900

    no match block

--- src/commons/Classifier.cpp
+++ src/commons/Classifier.cpp
@@ -533,6 +533,7 @@ void Classifier::analyseResultParallel(NcbiTaxonomy & ncbiTaxonomy, vector<Seque
     {
 #pragma omp for schedule(dynamic, 1)
         for(size_t i = 0; i < blockIdx; ++ i ){
+            cout<<"query length "<<seqSegments[matchBlocks[i].id].length<<endl;
             TaxID selectedLCA = chooseBestTaxon2(ncbiTaxonomy, seqSegments[matchBlocks[i].id].length, matchBlocks[i].id,
                                                 matchBlocks[i].start,
                                                 matchBlocks[i].end, matchList.data, queryList);
