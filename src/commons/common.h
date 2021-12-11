046aa503bc17beb9810c7469381feed207bc05fe
jaebeom <jbeom0731@gmail.com>
2021-12-11 22:08:26 +0900

    32

--- src/commons/common.h
+++ src/commons/common.h
@@ -6,7 +6,7 @@
 #define ADCLASSIFIER2_COMMON_H
 #include "NcbiTaxonomy.h"
 #define kmerBufSize 10000000000 // 10000000000 | 2860000000
-#define ThreadNum 32
+#define ThreadNum 1
 #define SplitNum 4096
 #define PRINT false
 
