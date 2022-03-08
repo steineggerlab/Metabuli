cbabdfc1d116f5a7c19617730e983081e81b272e
jaebeom <jbeom0731@gmail.com>
2022-03-08 18:21:11 +0900

    a

--- src/commons/common.h
+++ src/commons/common.h
@@ -4,7 +4,7 @@
 
 
 #include "NcbiTaxonomy.h"
-#define kmerBufSize 10000000000 // 10000000000 | 2860000000
+#define kmerBufSize 2860000000//10000000000 // 10000000000 |
 #define ThreadNum 32
 #define SplitNum 4096
 #define PRINT false
