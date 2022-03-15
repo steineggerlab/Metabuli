8ab03491dba23f068455fadb343a26e5390c8cac
jaebeom <jbeom0731@gmail.com>
2022-03-15 16:06:03 +0900

    small match buffer for each thread

--- src/commons/common.h
+++ src/commons/common.h
@@ -7,7 +7,7 @@
 #define kmerBufSize 10000000000 // 10000000000 | 286000000
 #define ThreadNum 32
 #define SplitNum 4096
-#define PRINT true
+#define PRINT false
 
 struct Sequence{
     Sequence(size_t start, size_t end, size_t length) : start(start), end(end), length(length) { }
