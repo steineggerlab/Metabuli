eeb899dc99eb7b2d07e115246f4a9975dafeb395
jaebeom <jbeom0731@gmail.com>
2022-04-29 23:34:35 +0900

    At least four matches for a species, 0.1 minimum

--- src/commons/Classifier.cpp
+++ src/commons/Classifier.cpp
@@ -1018,7 +1018,7 @@ int Classifier::getMatchesOfTheBestGenus_paired(vector<Match> &matchesForMajorit
             speciesDiffPosCnt = 0;
             lastPos = -1;
             lastIn = false;
-            while (currentSpecies == matchList[i].speciesTaxID && (i < end + 1)) {
+            while (currentSpecies == matchList[i+1].speciesTaxID && (i < end + 1)) {
                 if(currentSpecies == matchList[i + 1].speciesTaxID){
                     if(matchList[i].position + 3 <= matchList[i + 1].position){
                         filteredMatches.push_back(matchList[i]);
