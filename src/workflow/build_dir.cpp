9c7876525fb4c31688ab496b4b1d30a9ea0ef610
username <jbeom0731@gmail.com>
2022-07-12 17:49:57 +0900

    default parameter build_dir

--- src/workflow/build_dir.cpp
+++ src/workflow/build_dir.cpp
@@ -4,9 +4,15 @@
 #include <Command.h>
 #include <random>
 
+void setDefaults_build_dir(LocalParameters & par){
+    par.reducedAA = 0;
+    par.spaceMask = "11111111";
+}
+
 int build_dir(int argc, const char **argv, const Command &command)
 {
     LocalParameters &par = LocalParameters::getLocalInstance();
+    setDefaults_build_dir(par);
     par.parseParameters(argc, argv, command, true, Parameters::PARSE_ALLOW_EMPTY, 0);
 
     //Make files of differential indexing and information of k-mers
