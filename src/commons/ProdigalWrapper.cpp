b9bf949c4893fb184935625a63111dee141fc2f5
username <jbeom0731@gmail.com>
2022-09-08 13:56:00 +0900

    reduce printing

--- src/commons/ProdigalWrapper.cpp
+++ src/commons/ProdigalWrapper.cpp
@@ -268,9 +268,7 @@ void ProdigalWrapper::getPredictedGenes(char * genome){
         tweak_final_starts(genes, ng, nodes, nn, meta[max_phase].tinf);
         record_gene_data(genes, ng, nodes, meta[max_phase].tinf, num_seq);
     }
-//     if(1) {
-//         fprintf(stderr, "done! gene count: %d (%d bp)\n", ng, slen);
-//     }
+    fprintf(stderr, "done! gene count: %d (%d bp)\n", ng, slen);
 }
 
 int ProdigalWrapper::getNextSeq(char * line, int training) {
