b7347c342418400255fbcaf0d1cf25b619ccfe11
username <jbeom0731@gmail.com>
2022-09-21 23:21:12 +0900

    inclusion test for centrifuge

--- src/commons/common.h
+++ src/commons/common.h
@@ -6,7 +6,7 @@
 #define kmerBufSize 10'000'000'000  // 1'000'000'000 //286'000'000 //   10'000'000'000 | 286'000'000 (16 byte x 1 giga = 16 GB)
                                 // 10'000'000'000 -> build_dir 397G RAM
 #define SplitNum 4096
-#define PRINT true
+#define PRINT false
 
 #define likely(x) __builtin_expect((x),1)
 #define unlikely(x) __builtin_expect((x),0)
