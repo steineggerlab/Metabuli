b9f7196a16adc6fb2299ac7fdbd56d02a2833ca7
username <jbeom0731@gmail.com>
2022-09-29 17:35:14 +0900

    code for performance measure

--- src/workflow/inclusion_test_hiv.cpp
+++ src/workflow/inclusion_test_hiv.cpp
@@ -87,7 +87,15 @@ int inclusiontest_hiv(int argc, const char **argv, const Command &command){
     }
     fclose(taxIdFile);
 
+    vector<TaxID> genusTaxIdList;
+    ncbiTaxonomy.createTaxIdListAtRank(taxIdList, genusTaxIdList, "genus");
+    // Print TaxIDs in genusTaxIdList
+    for(int i = 0; i < genusTaxIdList.size(); i++){
+        cout << genusTaxIdList[i] << endl;
+    }
 
+    return 0;
+    
     unordered_map<TaxID, unsigned int> taxonCnt;
     for(TaxID x : taxIdList){
         taxonCnt[x] = 1;
