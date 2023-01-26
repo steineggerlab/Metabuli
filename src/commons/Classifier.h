2f45349fdf73c67bf5fa761cea8487844cf14a3f
username <jbeom0731@gmail.com>
2023-01-26 11:21:04 +0900

    fix bug

--- src/commons/Classifier.h
+++ src/commons/Classifier.h
@@ -175,6 +175,9 @@ protected:
     TaxonScore getBestGenusMatches2(vector<Match> &matchesForMajorityLCA, Match *matchList, size_t end,
                                    size_t offset, int queryLength);
 
+    TaxonScore getBestGenusMatches3(vector<Match> &matchesForMajorityLCA, Match *matchList, size_t end,
+                                    size_t offset, int queryLength);
+
     TaxonScore getBestGenusMatches(vector<Match> &matchesForMajorityLCA, Match *matchList, size_t end, size_t offset,
                                    int readLength1, int readLength2);
 
