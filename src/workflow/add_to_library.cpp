d77f6b18ca39b5abefd7206a668a5d019c86bebc
JaebeomKim0731 <jbeom0731@gmail.com>
2023-08-11 15:53:37 +0900

    support fna.gz files for add-to-library module

--- src/workflow/add_to_library.cpp
+++ src/workflow/add_to_library.cpp
@@ -5,7 +5,6 @@
 #include "KSeqWrapper.h"
 #include <iostream>
 #include "IndexCreator.h"
-#include <string>
 #include "FileUtil.h"
 
 using namespace std;
@@ -28,9 +27,8 @@ int addToLibrary(int argc, const char **argv, const Command &command){
     if (par.taxonomyPath == "DBDIR/taxonomy/") par.taxonomyPath = dbDir + "/taxonomy/";
     if (par.libraryPath == "DBDIR/library/") par.libraryPath = dbDir + "/library/";
 
-//    string libraryPath = dbDir + "/library";
     // If the library directory does not exist, create it
-    if (FileUtil::directoryExists(par.libraryPath.c_str()) == false) {
+    if (!FileUtil::directoryExists(par.libraryPath.c_str())) {
         FileUtil::makeDir(par.libraryPath.c_str());
     }
 
@@ -39,7 +37,6 @@ int addToLibrary(int argc, const char **argv, const Command &command){
     string nodes =  par.taxonomyPath + "/nodes.dmp";
     string merged =  par.taxonomyPath + "/merged.dmp";
     NcbiTaxonomy ncbiTaxonomy(names, nodes, merged);
-
 
     // Load file names
     ifstream fileListFile;
@@ -72,38 +69,23 @@ int addToLibrary(int argc, const char **argv, const Command &command){
         }
         cout << "done" << endl;
 
-        vector<SequenceBlock> sequences;
-        vector<string> unmapped;
+
         // Process each file
-        size_t numberOfFiles = fileNames.size();
-        for (size_t i = 0; i < numberOfFiles; ++i) {
-            sequences.clear();
-            string fileName = fileNames[i];
-
-            // Getting start and end position of each sequence
-            IndexCreator::getSeqSegmentsWithHead(sequences, fileName.c_str());
-
-            // Mmap the file
-            struct MmapedData<char> seqFile = mmapData<char>(fileName.c_str());
-            kseq_buffer_t buffer;
-            kseq_t *seq;
-
-            for (size_t j = 0; j < sequences.size(); ++j) {
-                buffer = {const_cast<char *>(&seqFile.data[sequences[j].start]),
-                          static_cast<size_t>(sequences[j].length)};
-                seq = kseq_init(&buffer);
-                kseq_read(seq);
+        vector<string> unmapped;
+        for (size_t i = 0; i < fileNames.size(); ++i) {
+            KSeqWrapper* kseq = KSeqFactory(fileNames[i].c_str());
+            while (kseq->ReadEntry()) {
+                const KSeqWrapper::KSeqEntry & e = kseq->entry;
 
                 // Extract accession and Remove the version number
-                string accession = string(seq->name.s);
+                string accession = string(e.name.s);
                 size_t pos = accession.find('.');
                 if (pos != string::npos) { accession = accession.substr(0, pos); }
 
                 // Skip if accession is not in the mapping file
                 if (acc2taxid.find(accession) == acc2taxid.end()) {
-                    cout << "During processing " << fileName << ", accession " << accession <<
+                    cout << "During processing " << fileNames[i] << ", accession " << accession <<
                          " is not found in the mapping file. It is skipped." << endl;
-                    kseq_destroy(seq);
                     unmapped.push_back(accession);
                     continue;
                 }
@@ -111,27 +93,26 @@ int addToLibrary(int argc, const char **argv, const Command &command){
                 // Get species taxID
                 int speciesTaxID = ncbiTaxonomy.getTaxIdAtRank(acc2taxid[accession], "species");
 
+                // Skip if species taxID is not found
                 if (speciesTaxID == 0) {
-                    cout << "During processing " << fileName << ", accession " << accession <<
+                    cout << "During processing " << fileNames[i] << ", accession " << accession <<
                          " is not matched to any species. It is skipped." << endl;
-                    kseq_destroy(seq);
                     continue;
                 }
 
-                // Write to file
+                // Write each sequence to file with species taxID as file name
                 FILE *file = fopen((dbDir + "/library/" + to_string(speciesTaxID) + ".fna").c_str(), "a");
-                fprintf(file, ">%s %s\n", seq->name.s, seq->comment.s);
-                fprintf(file, "%s\n", seq->seq.s);
+                fprintf(file, ">%s %s\n", e.name.s, e.comment.s);
+                fprintf(file, "%s\n", e.sequence.s);
                 fclose(file);
-
-                kseq_destroy(seq);
             }
-            munmap(seqFile.data, seqFile.fileSize + 1);
+            delete kseq;
         }
+
         // Write unmapped accession to file
         FILE *file = fopen((dbDir + "/unmapped.txt").c_str(), "w");
-        for (size_t i = 0; i < unmapped.size(); ++i) {
-            fprintf(file, "%s\n", unmapped[i].c_str());
+        for (const auto & i : unmapped) {
+            fprintf(file, "%s\n", i.c_str());
         }
         fclose(file);
     }
@@ -156,33 +137,20 @@ int addToLibrary(int argc, const char **argv, const Command &command){
             cerr << "Cannot open the mapping from assembly accession to tax ID" << endl;
         }
 
-        vector<SequenceBlock> sequences;
+        // Process each file
         vector<string> unmapped;
         regex regex1("(GC[AF]_[0-9]*\\.[0-9]*)");
-        // Process each file
-        size_t numberOfFiles = fileNames.size();
-        for (size_t i = 0; i < numberOfFiles; ++i) {
-            sequences.clear();
-            string fileName = fileNames[i];
-
-            // Getting start and end position of each sequence
-            IndexCreator::getSeqSegmentsWithHead(sequences, fileName.c_str());
-
-            // Mmap the file
-            struct MmapedData<char> seqFile = mmapData<char>(fileName.c_str());
-            kseq_buffer_t buffer;
-            kseq_t *seq;
-
+        for (size_t i = 0; i < fileNames.size(); ++i) {
             // Get assembly accession from file name using regex and remove the version number
             smatch match;
-            regex_search(fileName, match, regex1);
+            regex_search(fileNames[i], match, regex1);
             string assemblyID = match[0];
             size_t pos = assemblyID.find('.');
             if (pos != string::npos) { assemblyID = assemblyID.substr(0, pos); }
 
             // Skip if current assembly accession is not in the mapping file
             if (assembly2taxid.find(assemblyID) == assembly2taxid.end()) {
-                cout << "During processing " << fileName << ", accession " << assemblyID <<
+                cout << "During processing " << fileNames[i] << ", accession " << assemblyID <<
                      " is not found in the mapping file. It is skipped." << endl;
                 unmapped.push_back(assemblyID);
                 continue;
@@ -191,35 +159,27 @@ int addToLibrary(int argc, const char **argv, const Command &command){
             // Get species taxID
             int speciesTaxID = ncbiTaxonomy.getTaxIdAtRank(assembly2taxid[assemblyID], "species");
             if (speciesTaxID == 0) {
-                cout << "During processing " << fileName << ", accession " << assemblyID <<
+                cout << "During processing " << fileNames[i] << ", accession " << assemblyID <<
                      " is not matched to any species. It is skipped." << endl;
                 continue;
             }
 
-            for (size_t j = 0; j < sequences.size(); ++j) {
-                buffer = {const_cast<char *>(&seqFile.data[sequences[j].start]),
-                          static_cast<size_t>(sequences[j].length)};
-                seq = kseq_init(&buffer);
-                kseq_read(seq);
-
-                // Extract accession
-                string accession = string(seq->name.s);
-                acc2taxid[accession] = assembly2taxid[assemblyID];
-
+            KSeqWrapper* kseq = KSeqFactory(fileNames[i].c_str());
+            while (kseq->ReadEntry()){
+                const KSeqWrapper::KSeqEntry & e = kseq->entry;
                 // Write to file
-//                FILE *file = fopen((dbDir + "/library/" + to_string(speciesTaxID) + ".fna").c_str(), "a");
-//                fprintf(file, ">%s %s\n", seq->name.s, seq->comment.s);
-//                fprintf(file, "%s\n", seq->seq.s);
-//                fclose(file);
-
-                kseq_destroy(seq);
+                FILE *file = fopen((dbDir + "/library/" + to_string(speciesTaxID) + ".fna").c_str(), "a");
+                fprintf(file, ">%s %s\n", e.name.s, e.comment.s);
+                fprintf(file, "%s\n", e.sequence.s);
+                fclose(file);
             }
-            munmap(seqFile.data, seqFile.fileSize + 1);
+            delete kseq;
         }
+
         // Write unmapped accession to file
         FILE *file = fopen((dbDir + "/unmapped.txt").c_str(), "w");
-        for (size_t i = 0; i < unmapped.size(); ++i) {
-            fprintf(file, "%s\n", unmapped[i].c_str());
+        for (const auto & i : unmapped) {
+            fprintf(file, "%s\n", i.c_str());
         }
         fclose(file);
 
@@ -227,12 +187,12 @@ int addToLibrary(int argc, const char **argv, const Command &command){
         cout << "Write mapping from accession to taxonomy ID" << endl;
         file = fopen((dbDir + "/my.accession2taxid").c_str(), "w");
         fprintf(file, "accession\taccession.version\ttaxid\tgi");
-        for (auto it = acc2taxid.begin(); it != acc2taxid.end(); ++it) {
+        for (auto & it : acc2taxid) {
             // Get accession without a version number
-            string accession = it->first;
+            string accession = it.first;
             size_t pos = accession.find('.');
             if (pos != string::npos) { accession = accession.substr(0, pos);}
-            fprintf(file, "\n%s\t%s\t%d\t0", accession.c_str(), it->first.c_str(), it->second);
+            fprintf(file, "\n%s\t%s\t%d\t0", accession.c_str(), it.first.c_str(), it.second);
         }
         fclose(file);
     }
