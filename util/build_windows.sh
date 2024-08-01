281f62dd6a7e06683bc3ebd96f54936d6ef27b06
Jaebeom Kim <68528165+jaebeom-kim@users.noreply.github.com>
2024-08-01 15:16:47 +0900

    Update build_windows.sh: From SSE4.1 to SSE2

--- util/build_windows.sh
+++ util/build_windows.sh
@@ -8,8 +8,8 @@ if [ ! -d "$REPO" ]; then
     exit 1
 fi
 
-mkdir -p "$BUILD/build_sse41" && cd "$BUILD/build_sse41"
-cmake -DCMAKE_BUILD_TYPE=Release -DHAVE_TESTS=0 -DHAVE_MPI=0 -DHAVE_SSE4_1=1 -DBUILD_SHARED_LIBS=OFF -DCMAKE_EXE_LINKER_FLAGS="-static -static-libgcc -static-libstdc++" -DCMAKE_FIND_LIBRARY_SUFFIXES=".a" "$REPO"
+mkdir -p "$BUILD/build_sse2" && cd "$BUILD/build_sse2"
+cmake -DCMAKE_BUILD_TYPE=Release -DHAVE_TESTS=0 -DHAVE_MPI=0 -DHAVE_SSE2=1 -DBUILD_SHARED_LIBS=OFF -DCMAKE_EXE_LINKER_FLAGS="-static -static-libgcc -static-libstdc++" -DCMAKE_FIND_LIBRARY_SUFFIXES=".a" "$REPO"
 make -j 4
 mkdir -p "$BUILD/${BINARY_NAME}/bin"
 cp "src/${BINARY_NAME}" "$BUILD/${BINARY_NAME}/bin/${BINARY_NAME}"
