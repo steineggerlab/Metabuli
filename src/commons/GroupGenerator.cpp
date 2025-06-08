3cbfb61bd9fcdf05db3c667f0d4a3d2e0e2aaf95
LunaJang <lunajang@kakao.com>
2025-06-08 12:31:35 +0900

    주석 해제

--- src/commons/GroupGenerator.cpp
+++ src/commons/GroupGenerator.cpp
@@ -74,71 +74,68 @@ void GroupGenerator::startGroupGeneration(const LocalParameters &par) {
     cout << "thresholdK: " << thresholdK << endl;
     
     //Extract k-mers from query sequences and compare them to target k-mer DB
-    // while (!complete) {
-    //     tries++;
+    while (!complete) {
+        tries++;
 
-    //     // new code
-    //     if (tries == 1) {
-    //             cout << "Indexing query file ...";
-    //     }
-    //     queryIndexer->setBytesPerKmer(matchPerKmer);
-    //     queryIndexer->indexQueryFile(processedReadCnt);
-    //     const vector<QuerySplit> & queryReadSplit = queryIndexer->getQuerySplits();
+        // new code
+        if (tries == 1) {
+                cout << "Indexing query file ...";
+        }
+        queryIndexer->setBytesPerKmer(matchPerKmer);
+        queryIndexer->indexQueryFile(processedReadCnt);
+        const vector<QuerySplit> & queryReadSplit = queryIndexer->getQuerySplits();
 
-    //     if (tries == 1) {
-    //         totalSeqCnt = queryIndexer->getReadNum_1();
-    //         cout << "Done" << endl;
-    //         cout << "Total number of sequences: " << queryIndexer->getReadNum_1() << endl;
-    //         cout << "Total read length: " << queryIndexer->getTotalReadLength() <<  "nt" << endl;
-    //     }
+        if (tries == 1) {
+            totalSeqCnt = queryIndexer->getReadNum_1();
+            cout << "Done" << endl;
+            cout << "Total number of sequences: " << queryIndexer->getReadNum_1() << endl;
+            cout << "Total read length: " << queryIndexer->getTotalReadLength() <<  "nt" << endl;
+        }
 
-    //     // Set up kseq
-    //     KSeqWrapper* kseq1 = KSeqFactory(par.filenames[0].c_str());
-    //     KSeqWrapper* kseq2 = nullptr;
-    //     if (par.seqMode == 2) { kseq2 = KSeqFactory(par.filenames[1].c_str()); }
+        // Set up kseq
+        KSeqWrapper* kseq1 = KSeqFactory(par.filenames[0].c_str());
+        KSeqWrapper* kseq2 = nullptr;
+        if (par.seqMode == 2) { kseq2 = KSeqFactory(par.filenames[1].c_str()); }
 
-    //     // Move kseq to unprocessed reads
-    //     for (size_t i = 0; i < processedReadCnt; i++) {
-    //         kseq1->ReadEntry();
-    //         if (par.seqMode == 2) { kseq2->ReadEntry(); }
-    //     }
+        // Move kseq to unprocessed reads
+        for (size_t i = 0; i < processedReadCnt; i++) {
+            kseq1->ReadEntry();
+            if (par.seqMode == 2) { kseq2->ReadEntry(); }
+        }
 
-    //     for (size_t splitIdx = 0; splitIdx < queryReadSplit.size(); splitIdx++) {
-    //         // Allocate memory for query list
-    //         queryList.clear();
-    //         queryList.resize(queryReadSplit[splitIdx].end - queryReadSplit[splitIdx].start);
+        for (size_t splitIdx = 0; splitIdx < queryReadSplit.size(); splitIdx++) {
+            // Allocate memory for query list
+            queryList.clear();
+            queryList.resize(queryReadSplit[splitIdx].end - queryReadSplit[splitIdx].start);
 
-    //         // Allocate memory for query k-mer buffer
-    //         queryKmerBuffer.reallocateMemory(queryReadSplit[splitIdx].kmerCnt);
+            // Allocate memory for query k-mer buffer
+            queryKmerBuffer.reallocateMemory(queryReadSplit[splitIdx].kmerCnt);
 
-    //         // Initialize query k-mer buffer and match buffer
-    //         queryKmerBuffer.startIndexOfReserve = 0;
+            // Initialize query k-mer buffer and match buffer
+            queryKmerBuffer.startIndexOfReserve = 0;
 
-    //         // Extract query k-mers
-    //         kmerExtractor->extractQueryKmers(queryKmerBuffer,
-    //                                          queryList,
-    //                                          queryReadSplit[splitIdx],
-    //                                          par,
-    //                                          kseq1,
-    //                                          kseq2); 
-    //         kmerFileHandler->writeQueryKmerFile(queryKmerBuffer, outDir, numOfSplits, numOfThreads, processedReadCnt, jobId);
-    //         processedReadCnt += queryReadSplit[splitIdx].readCnt;
-    //         cout << "The number of processed sequences: " << processedReadCnt << " (" << (double) processedReadCnt / (double) totalSeqCnt << ")" << endl;
+            // Extract query k-mers
+            kmerExtractor->extractQueryKmers(queryKmerBuffer,
+                                             queryList,
+                                             queryReadSplit[splitIdx],
+                                             par,
+                                             kseq1,
+                                             kseq2); 
+            kmerFileHandler->writeQueryKmerFile(queryKmerBuffer, outDir, numOfSplits, numOfThreads, processedReadCnt, jobId);
+            processedReadCnt += queryReadSplit[splitIdx].readCnt;
+            cout << "The number of processed sequences: " << processedReadCnt << " (" << (double) processedReadCnt / (double) totalSeqCnt << ")" << endl;
 
-    //         numOfTatalQueryKmerCnt += queryKmerBuffer.startIndexOfReserve;
-    //     }
-    //     delete kseq1;
-    //     if (par.seqMode == 2) {
-    //         delete kseq2;
-    //     }
-    //     if (processedReadCnt == totalSeqCnt) {
-    //         complete = true;
-    //     }
-    // }   
-    processedReadCnt = 12048859;
-    numOfSplits = 6;
-    // makeGraph(outDir, numOfSplits, numOfThreads, numOfGraph, processedReadCnt, jobId);   
-    numOfGraph = 32;
+            numOfTatalQueryKmerCnt += queryKmerBuffer.startIndexOfReserve;
+        }
+        delete kseq1;
+        if (par.seqMode == 2) {
+            delete kseq2;
+        }
+        if (processedReadCnt == totalSeqCnt) {
+            complete = true;
+        }
+    }   
+    makeGraph(outDir, numOfSplits, numOfThreads, numOfGraph, processedReadCnt, jobId);   
 
     vector<pair<int, float>> metabuliResult;       
     metabuliResult.resize(processedReadCnt, make_pair(-1, 0.0f));
@@ -149,7 +146,7 @@ void GroupGenerator::startGroupGeneration(const LocalParameters &par) {
     queryGroupInfo.resize(processedReadCnt, -1);
     int dynamicGroupKmerThr = static_cast<int>(mergeRelations(outDir, numOfGraph, jobId, metabuliResult, thresholdK));
     
-    makeGroups(outDir, jobId, 120, groupInfo, queryGroupInfo);    
+    makeGroups(outDir, jobId, dynamicGroupKmerThr, groupInfo, queryGroupInfo);    
     saveGroupsToFile(groupInfo, queryGroupInfo, outDir, jobId);
     loadGroupsFromFile(groupInfo, queryGroupInfo, outDir, jobId);
     
